473d8dc5f0793cc8dcac7f1ea7253fcb9a500343
alemusu <alemusu2002@gmail.com>
2025-09-24 23:52:14 +0200

    al fin ya ta to bien

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -8,17 +8,14 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Hack:size=12" };
 static const char dmenufont[]       = "Hack:size=12";
-static const char col_main_bg[]     = "#000000";
-static const char col_unsel_border[]= "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_main[]        = "#b2b2b2"; //white
-//static const char col_main[]        = "#d82424"; //red
-static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_main_bg, col_unsel_border },
-	[SchemeSel]  = { col_gray4, col_main,  col_main  },
-};
+static const char col_gray1[]       = "";
+static const char col_gray2[]       = "";
+static const char col_gray3[]       = "";
+static const char col_gray4[]       = "";
+static const char col_cyan[]        = "";
+
+// set colors with pywal16 cache
+#include "/home/alemusu/.cache/wal/colors-wal-dwm.h"
 
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
@@ -60,7 +57,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_main_bg, "-nf", col_gray3, "-sb", col_main, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", NULL };
 static const char *termcmd[]  = { "kitty", NULL };
 static const char *upbrightness[]   = { "brightnessctl", "set", "+5%", NULL };
 static const char *downbrightness[] = { "brightnessctl", "set", "5%-", NULL };
